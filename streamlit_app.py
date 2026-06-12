95de85881d1e2c4095b1cbea66038c19b49c7f28
AsakusaKitsune <148590487+AsakusaKitsune@users.noreply.github.com>
2026-06-12 16:16:32 +1000

    Fix URL formatting in API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',
                              my_dataframe, max_selections=6) 
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
