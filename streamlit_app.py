b5572f4a7ced920db97e4c8a661d2cd773329f8b
AsakusaKitsune <148590487+AsakusaKitsune@users.noreply.github.com>
2026-06-12 20:56:40 +1000

    Update fruit options selection to include 'search_on'
    Added 'search_on' column to the fruit options selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,15 @@ st.write("The name on the smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+# st.text(smoothiefroot_response.json())
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:',
                              my_dataframe, max_selections=6) 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 if ingredients_list:
